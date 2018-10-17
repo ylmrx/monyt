bc0dc3e9c1991613b7c9d866331d42840756695b
Yoann Lamouroux <yoann.lamouroux@nbs-system.com>
2018-10-17 16:33:42 +0200

    first tests

--- monyt.py
+++ monyt.py
@@ -5,26 +5,11 @@ import logging
 import sys
 from logging.handlers import RotatingFileHandler 
 import time
+from pprint import pprint
 
 import multiprocessing
 from subprocess import check_output, CalledProcessError, Popen, PIPE, STDOUT
 
-def log_prepare(level, logfile, size, retention):
-    """
-    instantiate a logger according the given parameters
-    level, size, retention are self-explaining
-    """
-    logger = logging.getLogger()
-    formatter = logging.Formatter('%(asctime)s :: [%(levelname)s] :: %(message)s')
-    loglevel = logging.getLevelName(level)
-
-    logger.setLevel(loglevel)
-    logfile_handler = RotatingFileHandler(logfile, 'a', size, retention)
-    logfile_handler.setLevel(loglevel)
-    logfile_handler.setFormatter(formatter)
-    logger.addHandler(logfile_handler)
-
-    return logger
 
 class PingLoop(object):
     """
@@ -42,27 +27,70 @@ class PingLoop(object):
 
     def ping_loop(self):
         while True:
+            command =["ping", "-c", str(self.param['num']), "-W", str(self.param['timeout']), self.host]
+            p = Popen(command, stdout=PIPE, stderr=STDOUT)
+            p.wait()
             if self.pause_heartbeat == 0:
                 self.logger.info("waiting for next ping... %ds", self.param['nextping'])
                 time.sleep(self.param['nextping'])
-
-                command =["ping", "-c", str(self.param['num']), "-W", str(self.param['timeout']), self.host]
-
-                p = Popen(command, stdout=PIPE, stderr=STDOUT)
-                if p != 0:
+                if p.returncode != 0:
                     self.logger.critical("Failed to ping: %s", p.communicate())
-                    self.logger.info("Switching the remote route tables to here")
-                    for route in self.routes:
-                        logger.info("Treating route : %s", route.route_table_id)
-                        r = self.ec2.Route(route.route_table_id, '0.0.0.0/0')
-                        r.replace(InstanceId=self.local.id)
+                    switch_routes(self.routes, self.local, self.logger, self.ec2)
                     self.pause_heartbeat = 1
                     continue
-                self.logger.info("Successfull ping : %s, count=%s, timeout=%s" % 
+                else:
+                    self.logger.info("Successfull ping : %s, count=%s, timeout=%s" % 
                             (self.host, self.param['num'], self.param['timeout']))
             else:
-                self.logger.critical("The remote peer was down! Waiting 10' before I try to reach it again.")
-                time.sleep(600)
+                if p.returncode == 0:
+                    self.logger.critical("The remote_nat is back online!")
+                    self.pause_heartbeat  = 0
+                    continue
+                self.logger.critical("The remote peer was down! Waiting 1' before I try to reach it again.")
+                time.sleep(60)
+                
+
+
+def switch_routes(routes, instance, logger, ec2):
+    if len(routes) > 0:
+        logger.info("Switching the remote route tables to %s", instance.id)
+        for r in routes:
+            logger.info("Treating route : %s", r.route_table_id)
+            rt = ec2.Route(r.route_table_id, '0.0.0.0/0')
+            rt.replace(InstanceId=instance.id)
+    else:
+        logger.info("The route list is empty, no action needed")
+        
+def update_route_dict(vpc):
+    r = { 'local': [], 'remote': [], 'expected_local': [] } 
+    # local - route table currently pointing to local_nat instance
+    # remote - route table currently pointing to remote_nat instance
+    # expected_local - route tables mounted in current subnet (expected to be pointed to local_nat)
+    for rt in vpc.route_tables.all():
+        for attrs in rt.routes_attribute:
+            if not ('DestinationCidrBlock' in attrs.keys() and 
+                    'InstanceId' in attrs.keys()
+                   ):
+                continue
+            if attrs['DestinationCidrBlock'] == '0.0.0.0/0':
+                if attrs['InstanceId'] == local_nat.id:
+                    r['local'].append(rt)
+                if attrs['InstanceId'] == remote_nat.id:
+                    r['remote'].append(rt)
+    for sub in vpc.subnets.all():
+        if sub.availability_zone == local_zone:
+            for rt in vpc.route_tables.all():
+                for i in rt.associations_attribute:
+                    if 'SubnetId' in i.keys() and sub.subnet_id == i['SubnetId']:
+                            for attrs in rt.routes_attribute:
+                                if('DestinationCidrBlock' in attrs.keys() and
+                                   'InstanceId' in attrs.keys() and 
+                                   attrs['DestinationCidrBlock'] == '0.0.0.0/0'
+                                  ):
+                                    r['expected_local'].append(rt)
+    pprint(r)
+    return r
+
 
 def retrieve_tags(instance):
     tags = dict(map(
@@ -70,6 +98,23 @@ def retrieve_tags(instance):
         instance.tags
     ))
     return tags
+
+def log_prepare(level, logfile, size, retention):
+    """
+    instantiate a logger according the given parameters
+    level, size, retention are self-explaining
+    """
+    logger = logging.getLogger()
+    formatter = logging.Formatter('%(asctime)s :: [%(levelname)s] :: %(message)s')
+    loglevel = logging.getLevelName(level)
+
+    logger.setLevel(loglevel)
+    logfile_handler = RotatingFileHandler(logfile, 'a', size, retention)
+    logfile_handler.setLevel(loglevel)
+    logfile_handler.setFormatter(formatter)
+    logger.addHandler(logfile_handler)
+
+    return logger
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -95,10 +140,13 @@ if __name__ == "__main__":
     ec2_session = sess.resource('ec2')
 
     # search for my peer
-    #local_id = requests.get("http://169.254.169.254/latest/meta-data/instance-id").text # name
-    local_id = "i-060254f836f88c2a8"
+    local_id = requests.get("http://169.254.169.254/latest/meta-data/instance-id").text # name
+    local_zone = requests.get("http://169.254.169.254/latest/meta-data/placement/availability-zone").text
+    #local_id = "i-0ae65d966c805eb29"
+    #local_zone = "eu-west-1b"
     local_nat = ec2_session.Instance(local_id) # boto obj
     local_tags = retrieve_tags(local_nat)
+    
     remote_nat = "not_defined"
 
     if 'aws-nat' not in local_tags['nbs_roles']:
@@ -119,46 +167,30 @@ if __name__ == "__main__":
     # finding the route tables pointing to our nat-instances 
     vpc = ec2_session.Vpc(local_nat.vpc_id)
 
-    routes = {
-        'local': [],
-        'remote': []
-    }
 
-    for rt in vpc.route_tables.all():
-        for attrs in rt.routes_attribute:
-            if not ('DestinationCidrBlock' in attrs.keys() and 
-                    'InstanceId' in attrs.keys()
-                   ):
-                continue
-            if attrs['DestinationCidrBlock'] == '0.0.0.0/0':
-                if attrs['InstanceId'] == local_nat.id:
-                    routes['local'].append(rt)
-                if attrs['InstanceId'] == remote_nat.id:
-                    routes['remote'].append(rt)
-
-    print("Found local route tables:")
-    for l in routes['local']:
-        print(" -", l.route_table_id )
-    print("Found remote route tables:")
-    for r in routes['remote']:
-        print(" -", r.route_table_id)
+    routes = update_route_dict(vpc)
 
     remote_nat_ip = remote_nat.private_ip_address
     local_nat_ip = local_nat.private_ip_address
     logger.info("remote nat: %s  --- local nat: %s" % (local_nat_ip, remote_nat_ip))
     print("remote nat: %s  --- local nat: %s" % (local_nat_ip, remote_nat_ip))
+
+    logger.info("Hi friends ! I'm a NAT instance ! Let's get to work !")
+    switch_routes(routes['expected_local'], local_nat, logger, ec2_session)
+    routes = update_route_dict(vpc)
+
     logger.info("Spawning the monitoring thread")
 
-    ping = PingLoop("12.12.12.12", config['ping'], logger, routes['remote'], ec2_session, local_nat)
-    ping.ping_loop()
-#    try:
-#        ping.ping_loop()
-#    except:
-#        logger.critical("Exception caught")
-#    finally:
-#        logger.info("Shutting everything down")
-#        for procs in multiprocessing.active_children():
-#            logger.info("shutting down: %r", procs)
-#            procs.terminate()
-#            procs.join()
-#        logger.info("done.")
+    #ping = PingLoop("12.12.12.12", config['ping'], logger, routes['remote'], ec2_session, local_nat)
+    ping = PingLoop(remote_nat_ip, config['ping'], logger, routes['remote'], ec2_session, local_nat)
+    try:
+        ping.ping_loop()
+    except:
+        logger.critical("Exception caught")
+    finally:
+        logger.info("Shutting everything down")
+        for procs in multiprocessing.active_children():
+            logger.info("shutting down: %r", procs)
+            procs.terminate()
+            procs.join()
+        logger.info("done.")
